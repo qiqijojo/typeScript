b38677869f9f3b8b88097257f269335d6761fdca
jojo <774948439@qq.com>
2020-10-28 21:31:16 +0800

    泛型约束

--- TypeScript核心基础/src/js/index.ts
+++ TypeScript核心基础/src/js/index.ts
@@ -1,1 +1,10 @@
 console.log('00022222220000')
+interface LengthInterface{
+    length:number
+}
+let getArray = <T extends LengthInterface>(value:T, items:number = 5):T[]=>{
+    return new Array(items).fill(value);
+};
+let arr = getArray<string>('abc');
+// let arr = getArray<number>(6);
+let res = arr.map(item=>item.length);
